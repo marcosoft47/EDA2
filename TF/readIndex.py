ce1a60c041195148042728e97f22cf525cd25905
Mark <56490702+marcosoft47@users.noreply.github.com>
2025-07-02 17:52:36 -0300

    Implementado reconstrução por busca sequencial

--- TF/readIndex.py
+++ TF/readIndex.py
@@ -2,9 +2,18 @@ import cv2 as cv
 import numpy as np
 import avl
 
-def index2RGB(img: np.ndarray, colorMap: np.ndarray) -> np.ndarray:
+'''
+    Reconstroi a imagem usando máscara do NumPy
+'''
+def reconstruirMascara(img: np.ndarray, colorMap: np.ndarray) -> np.ndarray:
+    # Parece magia, mas é fancy indexing
+    # Para cada valor em img[i][j], pegue colorMap[valor].
     return colorMap[img]
 
+
+'''
+    Reconstroi a imagem usando uma árvore AVL
+'''
 def reconstruirArvore(img: np.ndarray, colorMap: np.ndarray) -> np.ndarray:
     arvore = avl.ArvoreAVL()
     reconstrucao = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
@@ -14,23 +23,40 @@ def reconstruirArvore(img: np.ndarray, colorMap: np.ndarray) -> np.ndarray:
     
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            res = arvore.buscar(img[i][j])
-            reconstrucao[i][j] = res
+            reconstrucao[i][j] = arvore.buscar(img[i][j])
+
+    # Forma mais eficiente, com máscaras do numpy
+    # for valor in np.unique(img):
+    #     mascara = (img == valor)
+    #     reconstrucao[mascara] = arvore.buscar(valor)
+
 
     return reconstrucao
 
+'''
+    Reconstroi a imagem usando busca sequencial
+'''
+def reconstruirSequencial(img: np.ndarray, colorMap: np.ndarray) -> np.ndarray:
+    reconstrucao = np.zeros((img.shape[0], img.shape[1], 3), np.uint8)
 
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            for k in range(len(colorMap)):
+                if img[i][j] == k:
+                    # gostaria de fazer reconstrucao[i][j] = colorMap[img[i][j]]
+                    # contudo, não seria uma busca sequencial
+                    reconstrucao[i][j] = colorMap[k]
+    return reconstrucao
 
 if __name__ == '__main__':
-
+    
     imgNp = np.load('npz/udesc.npz')
     colorMap = imgNp['Map']
     img = imgNp['Idx']
 
-    reconstrucao = index2RGB(img,colorMap)
-    reconstrucaoArvore = reconstruirArvore(img,colorMap)
-    print(np.unique(reconstrucaoArvore, axis=2))
-    cv.imshow('Numpy', reconstrucao)
-    cv.imshow('Arvore', reconstrucaoArvore)
+    # reconstrucao = index2RGB(img,colorMap)
+    # reconstrucao = reconstruirArvore(img,colorMap)
+    reconstrucao = reconstruirSequencial(img,colorMap)
+    cv.imshow('marcosoft47', reconstrucao)
     cv.waitKey()
     cv.destroyAllWindows()
